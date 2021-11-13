9af46ae8cac0990868f26bde8b790fc4412c21eb
ahhridoy <ahhridoyy28@gmail.com>
2021-11-13 20:26:53 +0600

    last added2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1xse.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
